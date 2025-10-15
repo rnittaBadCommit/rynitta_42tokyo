66361159fe983e31d40421a9937d55714011b15b
Ryushiro Nitta <rynitta@c1r7s4.42tokyo.jp>
2025-10-16 05:31:31 +0900

    remove unused func

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,23 +6,11 @@
 /*   By: rynitta <rynitta@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 02:54:29 by rynitta           #+#    #+#             */
-/*   Updated: 2025/10/16 02:59:14 by rynitta          ###   ########.fr       */
+/*   Updated: 2025/10/16 05:31:12 by rynitta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-int	_ft_strlen(const char *s)
-{
-	int	ret;
-
-	if (!s)
-		return (0);
-	ret = 0;
-	while (s[ret])
-		++ret;
-	return (ret);
-}
 
 size_t	ft_strlcat(char *dest, const char *src, size_t size)
 {
